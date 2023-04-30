f79ac938d20ba100d2eab2754d8382a20689c026
josephfranklin97 <josephfranklin.johnson@gmail.com>
2023-04-30 12:15:16 +0530

    To push the project

---
+++ src/test/java/org/tamil/Chennai.java
@@ -0,0 +1,19 @@
+package org.tamil;
+
+public class Chennai {
+
+	public void mylapore() {
+		System.out.println("Mylapore");
+	}
+
+	private void thuraipakkam() {
+		System.out.println("Thuraipakkam");
+
+	}
+
+	private void Navalur() {
+		System.out.println("Navalur");
+
+	}
+
+}
